f8697ee37f1d8f569f05e9b54eb9f2d0e8f2a206
Kosuke Tanabe <nabeta@fastmail.fm>
2016-11-16 02:21:03 +0900

    update spec file

--- spec/views/manifestations/show.html.erb_spec.rb
+++ spec/views/manifestations/show.html.erb_spec.rb
@@ -9,7 +9,7 @@ describe "manifestations/show" do
   end
 
   it "renders attributes in <p>" do
-    allow(view).to receive(:policy).and_return double(create?: true, update?: true)
+    allow(view).to receive(:policy).and_return double(show?: true, create?: true, update?: true)
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/1/)
