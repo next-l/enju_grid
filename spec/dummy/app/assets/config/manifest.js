7a9824aec172c461601fabf3917b3e50dcf289b4
Kosuke Tanabe <nabeta@fastmail.fm>
2020-03-04 02:02:32 +0900

    update dummy migration files

--- spec/dummy/app/assets/config/manifest.js
+++ spec/dummy/app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link unknown_resource.png
+//= link icons/page_white_text.png
+//= link icons/feed.png
